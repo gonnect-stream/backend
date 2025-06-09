e30cb18808af049caec9ad9c8e70ce93072db3cf
Mauricio Corrêa <mauriciocorrea@192.168.0.75>
2025-06-09 11:30:51 -0300

    TEste de nome

--- routes/upload.js
+++ routes/upload.js
@@ -12,7 +12,10 @@ const CLOUDFLARE_API_TOKEN = process.env.CLOUDFLARE_API_TOKEN;
 router.post("/", upload.single("file"), async (req, res) => {
   try {
     const file = req.file;
-    const { novoNome } = req.body;
+
+    const nomeEventoSlug = "leilao-bovino"; // ex: vindo do formulário
+    const timestamp = Date.now();
+    const nomeArquivo = `${nomeEventoSlug}-${timestamp}.jpg`;
 
     if (!file) {
       return res.status(400).json({ error: "Nenhum arquivo enviado." });
@@ -22,11 +25,11 @@ router.post("/", upload.single("file"), async (req, res) => {
       nome: file.originalname,
       tipo: file.mimetype,
       tamanhoKB: Math.round(file.size / 1024),
-      nomePersonalizado: req.body.novoNome,
+      nomePersonalizado: file.name,
     });
 
     const form = new FormData();
-    form.append("file", file.buffer, novoNome);
+    form.append("file", file.buffer, nomeArquivo);
 
     if (req.body.name) {
       form.append("name", req.body.name);
